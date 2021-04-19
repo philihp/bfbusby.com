fc1fa16d60035b8a72226f1c06ea0ef60e51c1b0
Philihp Busby <philihp@gmail.com>
2021-04-19 11:28:44 +0000

    add experience

--- pages/experience.js
+++ pages/experience.js
@@ -2,7 +2,14 @@ import React from 'react'
 import Layout from '../components/layouts/landing-layout'
 
 const Experience = () => {
-  return <Layout>Experience</Layout>
+  return (
+    <Layout>
+      <p>Aenean laoreet eros sed libero auctor faucibus.</p>
+      <p>Integer ut ex eget ante condimentum ornare eu sit amet lectus.</p>
+      <p>Cras rhoncus odio ac neque tempor, a tempor ex venenatis.</p>
+      <p>Nullam interdum lorem eu scelerisque fringilla.</p>
+    </Layout>
+  )
 }
 
 export default Experience
